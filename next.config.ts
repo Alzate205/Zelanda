725575afcfdccc96bdbd5a472a99952d78cc5b07
Samuel Alzate <samuelalzateberrio@gmail.com>
2026-05-28 09:51:21 -0500

    mejora  errores

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,21 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['gyburlhzvisgmdmfkqhx.supabase.co', 'lh3.googleusercontent.com'],
   },
+
+  // ← Muy importante para generar sourcemaps en producción
+  productionBrowserSourceMaps: true,
 };
 
 export default withSentryConfig(nextConfig, {
   org: 'zelanda',
   project: 'javascript-nextjs',
   silent: !process.env.CI,
+
   widenClientFileUpload: true,
   tunnelRoute: '/monitoring',
+
+  // ← Nueva sección recomendada
+  sourcemaps: {
+    deleteSourcemapsAfterUpload: true, // Elimina los .map del cliente después de subirlos
+  },
 });
